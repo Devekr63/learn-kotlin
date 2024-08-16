c58a14d7d3ea759705c83629f4b7d0556097ba33
Devendra <devekr12@gmail.com>
2024-08-17 01:33:39 +0530

    Exception handling

---
+++ Fundamental/src/main/kotlin/com/playground/exception_handling/HandlingExceptions.kt
@@ -0,0 +1,38 @@
+package com.playground.exception_handling
+
+import java.lang.Exception
+import kotlin.NullPointerException
+
+fun main(){
+    println(stringLength("Devendra"))
+    println(stringLength(null))
+
+    println(mergeName("devendra", "Kumar"))
+    println(mergeName("Dev", null))
+}
+
+//Try catch block is also an expression
+
+fun stringLength(s : String?) : Int?{
+    return try{
+        s!!.length
+    }catch(ex: Exception){
+        println(ex)
+        null
+    }
+}
+
+fun mergeName(firstName : String?, lastName : String?) : String?{
+    try{
+        firstName!!
+    }
+    catch(ex : NullPointerException){
+        throw NullPointerException("*** first name is null.")
+    }
+    try{
+        lastName!!
+    }catch(ex : NullPointerException){
+        throw NullPointerException("*** last name is null.")
+    }
+    return "${firstName.replaceFirstChar {firstName[0].uppercaseChar()}} ${lastName.replaceFirstChar { lastName[0].uppercaseChar() }}"
+}
